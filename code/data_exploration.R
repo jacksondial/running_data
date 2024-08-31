936c2f28b92b5f38384819f14e0218d28e400403
jacksondial <61251187+jacksondial@users.noreply.github.com>
2024-08-30 21:36:50 -0400

    Added data and initial cleaning

---
+++ code/data_exploration.R
@@ -0,0 +1,30 @@
+# Read in the data from strava
+
+pacman::p_load(
+  dplyr,
+  lubridate,
+  ggplot2
+)
+
+activity_dat <- read.csv("data/strava/activities.csv") |> 
+  select(where(~ !all(is.na(.))), -Athlete.Weight) |> # Remove all columns that are all missing values
+  filter(Activity.Type == "Run",
+         Elapsed.Time < 100000)
+
+labelled::generate_dictionary(activity_dat)
+
+
+activity_dat2 <- activity_dat |> 
+  mutate(date = mdy_hms(Activity.Date),
+         day = day(date),
+         month = month(date),
+         year = year(date),
+         distance_miles = Distance * 0.62137,
+         elapsed_minutes = Elapsed.Time / 60,
+         minutes = trunc(elapsed_minutes),
+         seconds = Elapsed.Time%%60) 
+
+ggplot(activity_dat2, aes(x = Elapsed.Time))+
+  geom_histogram()
+
+
