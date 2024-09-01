5122f260ad9251705e84325771100856fe7043db
jacksondial <61251187+jacksondial@users.noreply.github.com>
2024-08-31 22:22:33 -0400

    started another plot

--- code/plots.R
+++ code/plots.R
@@ -1,10 +1,27 @@
 # Plotting functions
-
+# x_var <- "Elapsed.Time"
 
 barplot_fun <- function(x_var){
   ggplot(activity_dat2, aes(x = !!sym(x_var)))+
-    geom_histogram()+
-    theme(panel.grid.minor = element_blank())
+    geom_histogram(aes(fill = "orange"))+
+    theme(panel.grid.minor = element_blank(),
+          legend.position = "none")+
+    scale_fill_manual(values = "orange2")
 }
 
+# corr_x_var <- "Elapsed.Time"
+# corr_y_var <- "Distance"
+# group_var <- "month"
+
+corr_plot <- function(corr_x_var, corr_y_var){
+  ggplot(activity_dat2)+
+    geom_point(aes(x = !!sym(corr_x_var), 
+                   y = !!sym(corr_y_var),
+                   color = as.factor(!!sym(group_var))),
+               size = 2.5,
+               alpha = .6)+
+    theme(panel.grid.minor = element_blank())+
+    scale_color_brewer(palette = "Paired")
+  
+}
 
