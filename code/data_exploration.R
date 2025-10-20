af2a139c35766c0543a31983305d8d30397bf52f
jacksondial <61251187+jacksondial@users.noreply.github.com>
2025-10-19 22:03:45 -0400

    Data update, added a new plot

--- code/data_exploration.R
+++ code/data_exploration.R
@@ -6,7 +6,7 @@ pacman::p_load(
   ggplot2
 )
 
-activity_dat <- read.csv("data/strava/activities.csv") |> 
+activity_dat <- read.csv("data/strava/October_19_2025/activities.csv") |> 
   select(where(~ !all(is.na(.))), -Athlete.Weight) |> # Remove all columns that are all missing values
   filter(Activity.Type == "Run",
          Elapsed.Time < 100000)
@@ -27,6 +27,12 @@ activity_dat2 <- activity_dat |>
          moving_total_c = paste0(moving_minutes, ":", moving_seconds),
          minutes = trunc(elapsed_minutes),
          seconds = Elapsed.Time%%60,
-         week = lubridate::week(date))
+         week = lubridate::week(date),
+         # The difference in week here is that this one below starts and ends on Mondays
+         # which is important for plotting weekly mileage since i structure my weekly
+         # schedule that way and not based on the actual week number of the year
+         week_monday = isoweek(date),
+         year_monday = isoyear(date) 
+         )
 
 saveRDS(activity_dat2, "data/activity_dat2.RDS")
--- code/plots.R
+++ code/plots.R
@@ -1,6 +1,8 @@
 # Plotting functions
 # x_var <- "Elapsed.Time"
 
+##### Total Barplot #####
+
 barplot_fun <- function(x_var){
   ggplot(app_dat, aes(x = !!sym(x_var)))+
     geom_histogram(aes(fill = running_palette[4]))+
@@ -13,6 +15,9 @@ barplot_fun <- function(x_var){
 # corr_x_var <- "Elapsed.Time"
 # corr_y_var <- "Distance"
 # group_var <- "month"
+
+
+##### Correlation Plot #####
 
 corr_plot <- function(corr_x_var, corr_y_var, group_var){
   ggplot(app_dat)+
@@ -29,3 +34,27 @@ corr_plot <- function(corr_x_var, corr_y_var, group_var){
   
 }
 
+##### Weekly plot #####
+
+# this_week <- activity_dat2 |> filter(year == 2025, week_monday == 42)
+weekly_bar_fun <- function(){
+  
+  weekly_dat <- activity_dat2 |> 
+    group_by(year, week_monday) |> 
+    summarise(weekly_mileage = sum(Distance)) |> 
+    mutate(above_40_m = ifelse(weekly_mileage > 40, T, F))
+  
+  ggplot(weekly_dat, aes(x = week_monday, y = weekly_mileage))+
+    geom_col(aes(fill = as.factor(year)))+
+    theme_bw()+
+    scale_fill_manual(values = running_palette)+
+    theme(panel.grid.major.x = element_blank(),
+          panel.grid.minor.x = element_blank()
+    )+
+    labs(
+      x = "Week of Year (Monday-Sunday)",
+      y = "Weekly Mileage",
+      fill = "Year")
+  
+  
+}
